527ee1330702fe07a3d73d7859938833629f39f8
SuperStartApp <webapp@superstart.it>
2026-06-10 18:48:19 +0200

    Rollback: rimossa PWA e ripristinata stabilità

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa' // Importa il plugin
 
+// Ho rimosso tutto quello che riguardava il plugin PWA
 export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA({ 
-      registerType: 'autoUpdate',
-      manifest: false // Abbiamo già creato il manifest.json a mano, quindi mettiamo false qui
-    })
-  ],
+  plugins: [react()],
 })
